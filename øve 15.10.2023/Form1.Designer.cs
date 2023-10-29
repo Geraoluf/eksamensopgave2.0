de63bec4d88c8785392112334106641cb2c7f328
gert Rasmussen <pjente@gmail.com>
2023-10-29 23:14:28 +0100

    Add project files.

---
+++ øve 15.10.2023/Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace øve_15._10._2023
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtMenu = new System.Windows.Forms.Button();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.txtvismenu = new System.Windows.Forms.ListBox();
+            this.bntvismenu = new System.Windows.Forms.Button();
+            this.txtsøgefelt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panelLogo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.panel1.Controls.Add(this.txtMenu);
+            this.panel1.Controls.Add(this.panelLogo);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(187, 497);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtMenu
+            // 
+            this.txtMenu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.txtMenu.FlatAppearance.BorderSize = 0;
+            this.txtMenu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.txtMenu.ForeColor = System.Drawing.Color.White;
+            this.txtMenu.Location = new System.Drawing.Point(0, 82);
+            this.txtMenu.Name = "txtMenu";
+            this.txtMenu.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.txtMenu.Size = new System.Drawing.Size(187, 54);
+            this.txtMenu.TabIndex = 1;
+            this.txtMenu.Text = "Menu";
+            this.txtMenu.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.txtMenu.UseVisualStyleBackColor = true;
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.panelLogo.Controls.Add(this.label3);
+            this.panelLogo.Controls.Add(this.label2);
+            this.panelLogo.Controls.Add(this.label1);
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(187, 82);
+            this.panelLogo.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(73, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "ENU";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Mongolian Baiti", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(24, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 14);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Food Registration";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(41, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 36);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "M";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(187, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(643, 82);
+            this.panel2.TabIndex = 1;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(504, 119);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(88, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Morgenmåltid";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // txtvismenu
+            // 
+            this.txtvismenu.FormattingEnabled = true;
+            this.txtvismenu.Location = new System.Drawing.Point(698, 416);
+            this.txtvismenu.Name = "txtvismenu";
+            this.txtvismenu.Size = new System.Drawing.Size(120, 69);
+            this.txtvismenu.TabIndex = 5;
+            // 
+            // bntvismenu
+            // 
+            this.bntvismenu.Location = new System.Drawing.Point(617, 462);
+            this.bntvismenu.Name = "bntvismenu";
+            this.bntvismenu.Size = new System.Drawing.Size(75, 23);
+            this.bntvismenu.TabIndex = 6;
+            this.bntvismenu.Text = "Søg menu";
+            this.bntvismenu.UseVisualStyleBackColor = true;
+            this.bntvismenu.Click += new System.EventHandler(this.bntvismenu_Click);
+            // 
+            // txtsøgefelt
+            // 
+            this.txtsøgefelt.Location = new System.Drawing.Point(617, 416);
+            this.txtsøgefelt.Name = "txtsøgefelt";
+            this.txtsøgefelt.Size = new System.Drawing.Size(75, 20);
+            this.txtsøgefelt.TabIndex = 7;
+            this.txtsøgefelt.Text = "txtsøgefelt";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(440, 113);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(40, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Gem";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(673, 278);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "se gemte menuer";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(673, 101);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(135, 160);
+            this.listBox1.TabIndex = 13;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(440, 159);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(40, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Gem";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(334, 162);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 15;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(334, 116);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 16;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(504, 142);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(60, 17);
+            this.radioButton2.TabIndex = 17;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Frokost";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(504, 165);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(77, 17);
+            this.radioButton3.TabIndex = 18;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Aftenmåltid";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(355, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Ingredienser";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(355, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Menunavn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 497);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtsøgefelt);
+            this.Controls.Add(this.bntvismenu);
+            this.Controls.Add(this.txtvismenu);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Ingredienser";
+            this.panel1.ResumeLayout(false);
+            this.panelLogo.ResumeLayout(false);
+            this.panelLogo.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button txtMenu;
+        private System.Windows.Forms.Panel panelLogo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.ListBox txtvismenu;
+        private System.Windows.Forms.Button bntvismenu;
+        private System.Windows.Forms.TextBox txtsøgefelt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ øve 15.10.2023/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace øve_15._10._2023
+{
+    public partial class Form1 : Form
+    {
+        List<string> ingredienserListe = new List<string>();
+
+        private Dictionary<string, string> myDictionary1 = new Dictionary<string, string>();
+
+        private Dictionary<string, string> myDictionary2 = new Dictionary<string, string>();
+        private Dictionary<string, string> myDictionary3 = new Dictionary<string, string>();
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        string valgtmåltid;
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (true)
+            {
+                valgtmåltid = "morgen";
+                
+            }
+            
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (true)
+            {
+                valgtmåltid = "frokost";
+                
+            }
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (true)
+            {
+                valgtmåltid = "aftenmåltid";
+                
+            }
+        }
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string value = string.Join(",", ingredienserListe);
+            ingredienserListe.Clear();
+            string key = textBox2.Text;
+
+
+
+
+            switch (valgtmåltid)
+            {
+                case "morgen":
+                    myDictionary1[key] = value;
+                    textBox1.Clear();
+                    textBox2.Clear();
+                    break;
+
+                case "frokost":
+                    myDictionary2[key] = value;
+                    textBox1.Clear();
+                    textBox2.Clear();
+                    break;
+
+                case "aftenmåltid":
+                    myDictionary3[key] = value;
+                    textBox1.Clear();
+                    textBox2.Clear();
+                    break;
+
+                default:
+                    MessageBox.Show(valgtmåltid);
+                    break;
+            }
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+        // viser de gemte menuer og værdier i en listbox
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            listBox1.Items.Clear(); // Fjern eventuelle tidligere elementer fra listBox1
+
+
+
+            if (valgtmåltid == "morgen")
+            {
+                foreach (var item in myDictionary1)
+                {
+                    listBox1.Items.Add($"{item.Key}: {item.Value}"); // Tilføj både nøglen og værdien til ListBox
+                }
+
+            }
+
+            else if (valgtmåltid == "frokost")
+            {
+
+                foreach (var item in myDictionary2)
+                {
+
+
+                    listBox1.Items.Add($"{item.Key}: {item.Value}"); // Tilføj både nøglen og værdien til ListBox
+
+
+                }
+            }
+
+            else if (valgtmåltid == "aftenmåltid")
+            {
+                foreach (var item in myDictionary3)
+                {
+                    listBox1.Items.Add($"{item.Key}: {item.Value}"); // Tilføj både nøglen og værdien til ListBox
+
+
+                }
+
+            }
+
+        }
+
+
+
+
+
+
+
+
+        // gemmer ingredienser i en List
+
+        private void button3_Click(object sender, EventArgs e)   
+        {
+
+            string ingredienser = textBox1.Text;
+
+           
+
+            ingredienserListe.Add(ingredienser);
+
+
+            textBox1.Clear();
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // søgefunktion - virker ikke endnu
+
+        private void bntvismenu_Click(object sender, EventArgs e)
+        {
+            string word = txtsøgefelt.Text;
+
+            if (myDictionary2.ContainsKey(word))
+            {
+                txtvismenu.Items.Clear(); // Ryd eventuelle tidligere elementer i listBox1
+                txtvismenu.Items.Add(myDictionary2[word]);
+
+            }
+            else
+            {
+                txtvismenu.Items.Clear();
+                txtvismenu.Items.Add("Nøgle ikke fundet");
+            }
+
+
+
+        }
+
+        
+    }
+}
